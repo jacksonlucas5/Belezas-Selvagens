b438a73b645423ce43dbe1801bd9b77e63489bb5
Jackson Lucas <jackson38582@gmail.com>
2023-05-07 11:46:27 -0300

    Init-accordion refatorato com class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import ScrollSuave from "./modules/scroll-suave.js";
 import initTabNav from "./modules/tab-nav.js";
-import initAccordion from "./modules/init-accordion.js";
+import Accordion from "./modules/init-accordion.js";
 import initForm from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropDown from "./modules/menu-dropdown.js";
@@ -10,15 +10,17 @@ import initAnimaNumero from "./modules/animacao-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initBitcoin from "./modules/fetch-bitcoin.js";
 
-initAnimacaoScroll();
-initDropDown();
-initMenu();
-initTabNav();
-initAccordion();
 const initScroll = new ScrollSuave('.menu a[href^="#"]');
 initScroll.addLinkEvent();
 console.log(initScroll);
 
+const initAccordion = new Accordion(".faq-lista > dt");
+initAccordion.init();
+
+initAnimacaoScroll();
+initDropDown();
+initMenu();
+initTabNav();
 initBitcoin();
 initAnimaNumero();
 initTooltip();
